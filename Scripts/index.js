91d189f6aa22f7de0bcfe2652bb9d87e41c14597
venkatesh.kocherlakota <venkatesh.kocherlakota@valgenesis.com>
2024-12-12 15:30:00 +0530

    Implemented download measurement function

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,7 +3,8 @@ import { createWriteStream } from 'fs';
 
 const url = 'http://localhost:5000/network/download'; // Replace with your server URL
 const outputFilePath = 'test-output.log';
-const CHUNK_SIZE_10KB = 10 * 1024; // 10KB
+const CHUNK_SIZE_BYTES = 10 * 1024; // 10KB
+const ONE_SECOND = 1000;
 
 async function downloadFile() {
   const { headers } = await axios.head(url);
@@ -16,7 +17,7 @@ async function downloadFile() {
   try {
     while (downloadedSize < fileSize) {
       const start = downloadedSize;
-      const end = Math.min(start + CHUNK_SIZE_10KB - 1, fileSize - 1);
+      const end = Math.min(start + CHUNK_SIZE_BYTES - 1, fileSize - 1);
       console.log('🐞 ~ file: index.js:22 ~ downloadFile ~ `bytes=${start}-${end}`', `bytes=${start}-${end}`)
       const { data } = await axios.get(url, {
         headers: {
@@ -32,7 +33,7 @@ async function downloadFile() {
         data.on('end', resolve);
       });
 
-      downloadedSize += CHUNK_SIZE_10KB;
+      downloadedSize += CHUNK_SIZE_BYTES;
     }
   } catch (e) {
     console.error('while loop error', e)
@@ -42,8 +43,70 @@ async function downloadFile() {
   console.log('Download complete');
 }
 
+/*
+function: measureDownloadSpeed
+Check how many chunks got downloaded in 1 sec and calculate the download speed
+*/
+
+async function measureDownloadSpeed() {
+  const { headers } = await axios.head(url);
+  const fileSize = parseInt(headers['content-length'], 10);
+  console.log('🐞 ~ file: index.js:13 ~ downloadFile ~ fileSize:', fileSize)
+  let downloadedSize = 0;
+  let numberOfDownloadedChunks = 0;
+  const writeStream = createWriteStream(outputFilePath);
+
+  try {
+    const startDate = Date.now();
+    let differenceInMilliseconds = 0;
+    while (downloadedSize < fileSize) {
+      differenceInMilliseconds = Date.now() - startDate;
+      if (differenceInMilliseconds >= ONE_SECOND) {
+        console.log('🐞 ~ file: index.js:65 ~ measureDownloadSpeed ~ differenceInMilliseconds:', differenceInMilliseconds)
+        break;
+      }
+      const start = downloadedSize;
+      const end = Math.min(start + CHUNK_SIZE_BYTES - 1, fileSize - 1);
+      // console.log('🐞 ~ file: index.js:22 ~ downloadFile ~ `bytes=${start}-${end}`', `bytes=${start}-${end}`)
+      const { data } = await axios.get(url, {
+        headers: {
+          Range: `bytes=${start}-${end}`,
+        },
+        responseType: 'stream',
+      });
+      data.pipe(writeStream, { end: false });
+
+
+      await new Promise((resolve) => {
+        // console.log('🐞 ~ file: index.js:36 ~ downloadFile ~ downloadedSize:', downloadedSize)
+        data.on('end', resolve);
+      });
+
+      downloadedSize += CHUNK_SIZE_BYTES;
+      numberOfDownloadedChunks++;
+    }
+  } catch (e) {
+    console.error('while loop error', e)
+  }
+
+  writeStream.end();
+  calculateDownloadSpeed({ chunksCount: numberOfDownloadedChunks, chunkSizeInBytes: CHUNK_SIZE_BYTES, timeInMS: ONE_SECOND });
+}
+
+function calculateDownloadSpeed(data) {
+  console.log('Number of chunks downloaded:', data.chunksCount);
+  console.log('Chunk size                 :', data.chunkSizeInBytes, 'B');
+  console.log('Time measured              :', data.timeInMS, 'ms');
+  const timeInSeconds = (data.timeInMS / 1000);
+  const dataSizeInKbps = (data.chunksCount * data.chunkSizeInBytes * 8) / 1024;
+  const downloadSpeedInKbps = dataSizeInKbps / timeInSeconds;
+  console.log('Download Speed             :', downloadSpeedInKbps, 'Kbps');
+  console.log('Download Speed             :', downloadSpeedInKbps / 1024, 'Mbps');
+}
+
 try {
-  await downloadFile()
+  // await downloadFile()
+  await measureDownloadSpeed()
 } catch (e) {
   console.error('error', e)
 }
--- controllers/NetworkBandwidthController.cs
+++ controllers/NetworkBandwidthController.cs
@@ -5,18 +5,23 @@ using System.Diagnostics;
 [Route("network")]
 public class NetworkBandwidthController : ControllerBase
 {
+    private const int HUNDRED_MILLISECONDS = 100;
+    private const int TEN_MILLISECONDS = 10;
     private readonly string _downloadFilePath;
     private readonly long _downloadFileSize;
+    private readonly int _delayInMilliseconds;
     public NetworkBandwidthController()
     {
         _downloadFilePath = Path.Combine(Environment.CurrentDirectory, "Data", "File-25MB.val");
         _downloadFileSize = new FileInfo(_downloadFilePath).Length;
+        // _delayInMilliseconds = HUNDRED_MILLISECONDS;
+        _delayInMilliseconds = TEN_MILLISECONDS;
     }
 
     [HttpHead("download")]
     public IActionResult DownloadChunkedHead()
     {
-        Response.Headers.Add("Content-Length", _downloadFileSize.ToString());
+        Response.Headers.Append("Content-Length", _downloadFileSize.ToString());
         return new EmptyResult();
     }
 
@@ -24,7 +29,7 @@ public class NetworkBandwidthController : ControllerBase
     public async Task<IActionResult> DownloadChunkedFileAsync()
     {
         var downloadFileStream = new FileStream(_downloadFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
-        Response.Headers.Add("Accept-Ranges", "bytes");
+        Response.Headers.Append("Accept-Ranges", "bytes");
         var rangeHeader = Request.Headers["Range"].ToString();
         if (string.IsNullOrEmpty(rangeHeader))
         {
@@ -37,15 +42,15 @@ public class NetworkBandwidthController : ControllerBase
         
         var contentLength = end - start + 1;
         Response.StatusCode = 206;
-        Response.Headers.Add("Content-Range", $"bytes {start}-{end}/{_downloadFileSize}");
+        Response.Headers.Append("Content-Range", $"bytes {start}-{end}/{_downloadFileSize}");
 
         downloadFileStream.Seek(start, SeekOrigin.Begin);
 
         var buffer = new byte[contentLength]; // Expecting 10 KB buffer size in ideal scenario
         var bytesRead = 0;
-        var stopwatch = new Stopwatch();
         await downloadFileStream.ReadAsync(buffer, 0, buffer.Length);
         await Response.Body.WriteAsync(buffer, 0, bytesRead);
+        await Task.Delay(_delayInMilliseconds);
         return new EmptyResult();
     }
 }
