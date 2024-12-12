f963b7e110339fb5714cd8fb6bde8fccd12935a8
venkatesh.kocherlakota <venkatesh.kocherlakota@valgenesis.com>
2024-12-12 16:35:39 +0530

    client side cleanup

--- Scripts/index.js
+++ Scripts/index.js
@@ -12,9 +12,9 @@ Check how many chunks got downloaded in 1 sec and calculate the download speed
 If we didn't fully receive a single chunk in a second, network speed is less than 80Kbps
 */
 async function measureDownloadSpeed() {
-  const { headers } = await axios.head(apiURL);
-  const totalServerFileSize = parseInt(headers['content-length'], 10);
+  const totalServerFileSize = await getServerFileSize();
   console.log('🐞 ~ file: index.js:13 ~ downloadFile ~ fileSize:', totalServerFileSize)
+
   let sizeDownloadedTillNow = 0;
   let numberOfDownloadedChunks = 0;
   // TODO: To create a unique file when making multiple calls to avoid concurrency issues.
@@ -23,24 +23,29 @@ async function measureDownloadSpeed() {
   try {
     const startTimestamp = Date.now();
     let timeElapsedInMilliseconds = 0;
+
     while (sizeDownloadedTillNow < totalServerFileSize) {
       timeElapsedInMilliseconds = Date.now() - startTimestamp;
       if (timeElapsedInMilliseconds >= ONE_SECOND) {
         console.log('🐞 ~ file: index.js:67 ~ measureDownloadSpeed ~ timeElapsedInMilliseconds:', timeElapsedInMilliseconds)
         break;
       }
+
       const start = sizeDownloadedTillNow;
       const end = Math.min(start + CHUNK_SIZE_BYTES - 1, totalServerFileSize - 1);
+
       const { data } = await axios.get(apiURL, {
         headers: {
           Range: `bytes=${start}-${end}`,
         },
         responseType: 'stream',
       });
+      // write data to temporary file
       data.pipe(tempFileWriteStream, { end: false });
       await new Promise((resolve) => {
         data.on('end', resolve);
       });
+
       sizeDownloadedTillNow += CHUNK_SIZE_BYTES;
       numberOfDownloadedChunks++;
     }
@@ -53,18 +58,24 @@ async function measureDownloadSpeed() {
   return { chunksCount: numberOfDownloadedChunks, chunkSizeInBytes: CHUNK_SIZE_BYTES, timeInMS: ONE_SECOND };
 }
 
+async function getServerFileSize () {
+  // No need to make this call if file size is fixed and won't change across releases
+  const { headers } = await axios.head(apiURL);
+  return parseInt(headers['content-length'], 10);
+}
+
 function calculateDownloadSpeed(data) {
-  console.log('Number of chunks downloaded:', data.chunksCount);
-  console.log('Chunk size                 :', data.chunkSizeInBytes, 'B');
-  console.log('Time measured              :', data.timeInMS, 'ms');
+  console.log('Number of chunks downloaded  :', data.chunksCount);
+  console.log('Chunk size                   :', data.chunkSizeInBytes, 'B');
+  console.log('Time measured                :', data.timeInMS, 'ms');
 
   const timeInSeconds = (data.timeInMS / 1000);
   const dataSizeInKbps = (data.chunksCount * data.chunkSizeInBytes * 8) / 1024;
   const downloadSpeedInKbps = dataSizeInKbps / timeInSeconds;
   const downloadSpeedInMbps = downloadSpeedInKbps / 1024;
 
-  console.log('Download Speed             :', downloadSpeedInKbps, 'Kbps');
-  console.log('Download Speed             :', downloadSpeedInMbps, 'Mbps');
+  console.log('Download Speed               :', downloadSpeedInKbps, 'Kbps');
+  console.log('Download Speed               :', downloadSpeedInMbps, 'Mbps');
 
   return { speedKbps: downloadSpeedInKbps, speedMbps: downloadSpeedInMbps };
 }
@@ -81,9 +92,10 @@ try {
   const avgDownloadSpeedKbps = (calculationONE.speedKbps + calculationTWO.speedKbps + calculationTHREE.speedKbps) / 3;
   const avgDownloadSpeedMbps = (calculationONE.speedMbps + calculationTWO.speedMbps + calculationTHREE.speedMbps) / 3;
 
-  console.log('Average Download Speed: ', avgDownloadSpeedKbps, 'Kbps', avgDownloadSpeedMbps, 'Mbps');
+  console.log('Average Download Speed       : ', avgDownloadSpeedKbps, 'Kbps');
+  console.log('Average Download Speed       : ', avgDownloadSpeedMbps, 'Mbps');
   console.log();
-  console.log("Includes latency in the network.");
+  console.log("Note: Includes latency in the network.");
 } catch (e) {
   console.error('error', e)
 }
