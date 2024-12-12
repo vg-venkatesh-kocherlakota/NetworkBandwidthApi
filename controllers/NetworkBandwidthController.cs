b57191e2d60c5f6e172f01cca6c60514ed2c06cd
venkatesh.kocherlakota <venkatesh.kocherlakota@valgenesis.com>
2024-12-12 16:04:34 +0530

    backend cleanup

--- controllers/NetworkBandwidthController.cs
+++ controllers/NetworkBandwidthController.cs
@@ -7,28 +7,33 @@ public class NetworkBandwidthController : ControllerBase
 {
     private const int HUNDRED_MILLISECONDS = 100;
     private const int TEN_MILLISECONDS = 10;
-    private readonly string _downloadFilePath;
-    private readonly long _downloadFileSize;
+    private const int FIVE_MILLISECONDS = 5;
+    private readonly string _filePathForDownloadSpeed;
+    private readonly long _totalFileSize;
     private readonly int _delayInMilliseconds;
     public NetworkBandwidthController()
     {
-        _downloadFilePath = Path.Combine(Environment.CurrentDirectory, "Data", "File-25MB.val");
-        _downloadFileSize = new FileInfo(_downloadFilePath).Length;
-        // _delayInMilliseconds = HUNDRED_MILLISECONDS;
-        _delayInMilliseconds = TEN_MILLISECONDS;
+        _filePathForDownloadSpeed = Path.Combine(Environment.CurrentDirectory, "Data", "File-25MB.val");
+        _totalFileSize = new FileInfo(_filePathForDownloadSpeed).Length;
+#if DEBUG
+        _delayInMilliseconds = FIVE_MILLISECONDS;
+#else
+        _delayInMilliseconds = 0;
+#endif
     }
 
     [HttpHead("download")]
     public IActionResult DownloadChunkedHead()
     {
-        Response.Headers.Append("Content-Length", _downloadFileSize.ToString());
+        Response.Headers.Append("Content-Length", _totalFileSize.ToString());
         return new EmptyResult();
     }
 
     [HttpGet("download")]
     public async Task<IActionResult> DownloadChunkedFileAsync()
     {
-        var downloadFileStream = new FileStream(_downloadFilePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+        var downloadFileStream = new FileStream(_filePathForDownloadSpeed, FileMode.Open, FileAccess.Read, FileShare.Read);
+
         Response.Headers.Append("Accept-Ranges", "bytes");
         var rangeHeader = Request.Headers["Range"].ToString();
         if (string.IsNullOrEmpty(rangeHeader))
@@ -38,11 +43,11 @@ public class NetworkBandwidthController : ControllerBase
 
         var range = rangeHeader.Replace("bytes=", "").Split('-');
         var start = int.Parse(range[0]);
-        var end = range.Length > 1 && !string.IsNullOrEmpty(range[1]) ? int.Parse(range[1]) : _downloadFileSize - 1;
-        
+        var end = range.Length > 1 && !string.IsNullOrEmpty(range[1]) ? int.Parse(range[1]) : _totalFileSize - 1;
+
         var contentLength = end - start + 1;
         Response.StatusCode = 206;
-        Response.Headers.Append("Content-Range", $"bytes {start}-{end}/{_downloadFileSize}");
+        Response.Headers.Append("Content-Range", $"bytes {start}-{end}/{_totalFileSize}");
 
         downloadFileStream.Seek(start, SeekOrigin.Begin);
 
@@ -50,7 +55,11 @@ public class NetworkBandwidthController : ControllerBase
         var bytesRead = 0;
         await downloadFileStream.ReadAsync(buffer, 0, buffer.Length);
         await Response.Body.WriteAsync(buffer, 0, bytesRead);
+
+#if DEBUG
         await Task.Delay(_delayInMilliseconds);
+#endif
+
         return new EmptyResult();
     }
 }
